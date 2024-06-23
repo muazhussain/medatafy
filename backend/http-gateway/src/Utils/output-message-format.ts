bc4bb141fd0dee1783d4b8833cd1a71132615a2a
muazhussain <muaz.self@gmail.com>
2024-06-23 23:52:09 +0600

    Add
    Common Utility Class Add

---
+++ backend/http-gateway/src/Utils/output-message-format.ts
@@ -0,0 +1,42 @@
+import { HttpException, HttpStatus } from '@nestjs/common';
+
+export function commonResponse(success: boolean, message: string, payload: any = null) {
+    if (success) {
+        return { success, message, data: payload };
+    } else {
+        let httpStatus = HttpStatus.BAD_REQUEST;
+        if (payload.status === HttpStatus.UNAUTHORIZED) {
+            httpStatus = HttpStatus.UNAUTHORIZED;
+        } else if (payload.status === HttpStatus.NOT_ACCEPTABLE) {
+            httpStatus = HttpStatus.NOT_ACCEPTABLE;
+        }
+
+        throw new HttpException(
+            {
+                success,
+                message: payload.message || message,
+                error: payload.message,
+            },
+            httpStatus,
+        );
+    }
+}
+
+export function listFilterResponse(
+    success: boolean,
+    message: string,
+    total: number,
+    take: number,
+    page: number,
+    data: any,
+) {
+    const res = {
+        success,
+        message,
+        total,
+        page,
+        take,
+        data,
+    };
+    return res;
+}
