5a449b323d799f0d7a70902138c22954ef7fe039
muazhussain <muaz.self@gmail.com>
2024-07-27 23:49:15 +0600

    Update
    App Module update.

--- backend/patient/src/app.module.ts
+++ backend/patient/src/app.module.ts
@@ -4,11 +4,14 @@ import { NatsClientModule } from './nats-client/nats-client.module';
 import { MedicalReportModule } from './medical-report/medical-report.module';
 import { PatientModule } from './patient/patient.module';
 import { DoctorAppointmentModule } from './doctor-appointment/doctor-appointment.module';
+import { HospitalAppointmentModule } from './hospital-appointment/hospital-appointment.module';
+import { PrescriptionModule } from './prescription/prescription.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
+      host: 'localhost',
       port: 5432,
       username: 'muaz',
       password: '123',
@@ -20,6 +23,8 @@ import { DoctorAppointmentModule } from './doctor-appointment/doctor-appointment
     MedicalReportModule,
     PatientModule,
     DoctorAppointmentModule,
+    HospitalAppointmentModule,
+    PrescriptionModule,
   ],
   controllers: [],
   providers: [],
