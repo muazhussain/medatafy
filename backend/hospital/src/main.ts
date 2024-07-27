9075346a07461d0bbf1717790f85eaf50543ba00
muazhussain <muaz.self@gmail.com>
2024-07-27 23:50:28 +0600

    Update
    main update

--- backend/hospital/src/main.ts
+++ backend/hospital/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import AppDataSource from './data-source';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  console.log('Hospital Microservice is Running!');
+  await AppDataSource.initialize();
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.NATS,
+      options: {
+        servers: ['nats://nats'],
+      },
+    },
+  );
+  await app.listen();
 }
 bootstrap();
