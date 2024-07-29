cca41428e1fbf055b97e6085443c57922adc8c29
muazhussain <muaz.self@gmail.com>
2024-07-29 23:22:41 +0600

    Update
    Connected to environment.

--- backend/doctor/src/app.module.ts
+++ backend/doctor/src/app.module.ts
@@ -1,22 +1,47 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { NatsClientModule } from './nats-client/nats-client.module';
 import { DoctorAppointmentModule } from './doctor-appointment/doctor-appointment.module';
 import { DoctorModule } from './doctor/doctor.module';
 import { PrescriptionModule } from './prescription/prescription.module';
+import { HospitalAppointmentEntity } from './other-entities/hospital-appointment.entity';
+import { HospitalEntity } from './other-entities/hospital.entity';
+import { MedicalReportEntity } from './other-entities/medical-report.entity';
+import { MedicalTestEntity } from './other-entities/medical-test.entity';
+import { MedicineEntity } from './other-entities/medicine.entity';
+import { PatientEntity } from './other-entities/patient.entity';
+import { UserEntity } from './other-entities/user.entity';
 
 @Module({
   imports: [
+    // TypeOrmModule.forRoot({
+    //   type: 'postgres',
+    //   host: 'localhost',
+    //   port: 5432,
+    //   username: 'muaz',
+    //   password: '123',
+    //   database: 'medatafy_db',
+    //   autoLoadEntities: true,
+    //   synchronize: true,
+    // }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      port: 5432,
-      username: 'muaz',
-      password: '123',
-      database: 'medatafy_db',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT, 10),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
       autoLoadEntities: true,
-      synchronize: true,
+      synchronize: process.env.SYNCHRONIZE === 'true',
     }),
-    NatsClientModule,
+    TypeOrmModule.forFeature([
+      HospitalAppointmentEntity,
+      HospitalEntity,
+      MedicalReportEntity,
+      MedicalTestEntity,
+      MedicineEntity,
+      PatientEntity,
+      UserEntity,
+    ]),
     DoctorAppointmentModule,
     DoctorModule,
     PrescriptionModule,
