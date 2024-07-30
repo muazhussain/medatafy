fb26cb175aa6cc8b4c5b38e058e4c1e319125e04
muazhussain <muaz.self@gmail.com>
2024-07-30 21:11:49 +0600

    Update
    Port specified

--- backend/doctor/src/main.ts
+++ backend/doctor/src/main.ts
@@ -2,18 +2,29 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import AppDataSource from './data-source';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   await AppDataSource.initialize();
+
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
       transport: Transport.NATS,
       options: {
-        servers: ['nats://nats'],
+        servers: ['nats://nats:4222'],
       },
     },
   );
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+      whitelist: true,
+    }),
+  );
+
   await app.listen().then(() => console.log(`Doctor Microservice is running`));
 }
 bootstrap();
